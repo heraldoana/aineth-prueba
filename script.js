f73f06ae7ab1f99e572a85459d13f3be61f9fb83
Heraldo Ana <heraldoana91@gmail.com>
2023-09-27 18:09:32 -0300

    second commit aineth

--- script.js
+++ script.js
@@ -1,59 +1,93 @@
+//cambiar idiomas con botones (banderitas)
+/* 
+let idiomaEs = document.getElementById('idiomaEs')
+let idiomaEn = document.getElementById('idiomaEn')
+idiomaEn.onclick = () => cambiarIdiomaEn ()
+function cambiarIdiomaEn() {
+    changeTextEnglish()
+}
+idiomaEs.onclick = () => cambiarIdiomaEs ()
+function cambiarIdiomaEs() {
+    changeTextSpanish()
+}
 
-document.addEventListener('DOMContentLoaded', function () {
-    const aiText = document.getElementById('aiText');
-    let currentText = 0;
-    const textOptions = ["partner", "friend", "college"];
+let aiText = document.getElementById('aiText');
+let currentText = 0
+const textOptionsEnglish = ["partner", "friend", "college"]
+const textOptionsSpanish = ["compañeros", "amigos", "colegio"]
+function changeTextEnglish() {
+    currentText = (currentText + 1) % textOptionsEnglish.length
+    aiText.textContent = `The AI ${textOptionsEnglish[currentText]} for businesses and people.`
+    setInterval(changeTextEnglish, 3000) // Cambia cada 3 segundos (3000 milisegundos)
+}
+function changeTextSpanish() {
+    currentText = (currentText + 1) % textOptionsSpanish.length
+    aiText.textContent = `La inteligencia artificial de ${textOptionsSpanish[currentText]} para trabajos y personas.`
+    setInterval(changeTextSpanish, 3000); // Cambia cada 3 segundos (3000 milisegundos)
+} */
 
-    // Función para cambiar el texto automáticamente
-    function changeText() {
-        currentText = (currentText + 1) % textOptions.length;
-        aiText.textContent = `The AI ${textOptions[currentText]} for businesses and people.`;
+
+/* // Cambiar el comportamiento del bot según el idioma
+const inicioSesionBtn = document.getElementById('provebot')
+inicioSesionBtn.addEventListener('click', function () {
+    if (language === 'es') {
+        // Lógica para abrir el bot de aineth.ai en español
+    } else if (language === 'en') {
+        // Lógica para abrir el bot de aineth.ai en inglés
     }
-    setInterval(changeText, 3000); // Cambia cada 3 segundos (3000 milisegundos)
+}) */
+
+document.addEventListener('DOMContentLoaded', function () {
+    const titulo = document.getElementById('aiText');
+
+
+    const palabras = ['partner', 'friend', 'college'];
+    let indicePalabra = 0;
+
+    // Función para cambiar la palabra y color
+    function cambiarPalabra() {
+        titulo.textContent = titulo.textContent.replace(palabras[indicePalabra], palabras[(indicePalabra + 1) % palabras.length]);
+        titulo.classList.toggle('blue-text');
+        indicePalabra = (indicePalabra + 1) % palabras.length;
+    }
+
+    // Cambiar la palabra cada 2 segundos
+    setInterval(cambiarPalabra, 2000);
 });
 
 document.addEventListener('DOMContentLoaded', function () {
-    const language = navigator.language.substring(0, 2);
-
-    // Cambiar el enlace del formulario de Google según el idioma
-    const buttonLogin = document.getElementById('login');
-    buttonLogin.addEventListener('click', function () {
-        const urlForm = language === 'es' ? 'URL_del_Formulario_en_español' : 'URL_del_Formulario_en_inglés';
-        window.open(urlForm, '_blank');
-    });
-
-    // Cambiar el comportamiento del bot según el idioma
-    const inicioSesionBtn = document.getElementById('provebot');
-    inicioSesionBtn.addEventListener('click', function () {
-        if (language === 'es') {
-            // Lógica para abrir el bot de aineth.ai en español
-        } else if (language === 'en') {
-            // Lógica para abrir el bot de aineth.ai en inglés
-        }
-    });
-});
-
-document.addEventListener('DOMContentLoaded', function () {
-    const inicioSesionBtn = document.getElementById('inicioSesionBtn');
-    const formularioSesion = document.getElementById('formularioSesion');
-    const usernameInput = document.getElementById('username');
-    const accederBtn = document.getElementById('accederBtn');
-
+    const formBtnEn = document.getElementById('formBtnEn')
+    const formularioSesion = document.getElementById('formularioSesion')
+    const usernameInput = document.getElementById('username')
+    const accederBtn = document.getElementById('accederBtn')
+    const userLabel = document.getElementById('userLabel')
+    
     // Lista de nombres de usuario válidos
-    const usuariosValidos = ['usuario1', 'usuario2', 'usuario3'];
+    const usuariosValidos = ['usuario1', 'usuario2', 'usuario3']
 
-    inicioSesionBtn.addEventListener('click', function () {
+    formBtnEn.addEventListener('click', function () {
         // Mostrar el formulario de inicio de sesión
-        formularioSesion.classList.remove('hidden');
+        formularioSesion.classList.remove('hidden')
     });
+
+    formBtnEs.addEventListener('click', function() {
+        formularioSesion.classList.remove('hidden')
+        userLabel.textContent = 'Usuario'
+        const inputElement = document.getElementById('username');
+        const nuevoPlaceholder = 'Ingrese el usuario';
+        inputElement.placeholder = nuevoPlaceholder;
+        accederBtn.textContent = 'Iniciar Sesion'
+    });
+
+
 
     accederBtn.addEventListener('click', function () {
-        const username = usernameInput.value;
-        
+        const username = usernameInput.value
+
         // Verificar si el nombre de usuario está en la lista de usuarios válidos
         if (usuariosValidos.includes(username)) {
             // Redirigir al usuario a la URL de destino (puedes modificar la URL)
-            window.location.href = 'URL_de_destino?usuario=' + username;
+            window.location.href = 'URL_de_destino?usuario=' + username
         } else {
             alert('The user entered is incorrect. please check');
         }
